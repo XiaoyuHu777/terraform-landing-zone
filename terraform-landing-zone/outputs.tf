43d1fb7e40e0caf604a99e2f4c0d05eb4b57eb50
XiaoyuHu777 <xiaoyu.hu@stonybrook.edu>
2025-03-31 12:48:09 -0400

    fix: format terraform files

--- terraform-landing-zone/outputs.tf
+++ terraform-landing-zone/outputs.tf
@@ -1,3 +1,3 @@
-output "azurerm_resource_group_name"{
-    value = azurerm_resource_group.landing_zone.name
+output "azurerm_resource_group_name" {
+  value = azurerm_resource_group.landing_zone.name
 }
